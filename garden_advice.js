4cfa7bcdb05289e5d3a2add9586e5d88b95619a6
morahapn <pnmorahanye@gmail.com>
2026-01-11 08:57:22 +0200

    Add files via upload

---
+++ garden_advice.js
@@ -0,0 +1,9 @@
+// This app gives gardening tips based on month and season
+
+// TODO: Create a function to return tips for a given month
+// TODO: Replace hardcoded tips with a data structure
+// TODO: Add comments to explain the code
+
+const januaryTips = "Start pruning and preparing your garden beds.";
+
+console.log(januaryTips);
